2664807205a70bb085c2585e5638d5943bfef4ab
Tanishq Tyagi <tyagitanishq45@gmail.com>
2024-08-06 21:46:44 +0530

    responsive hai!!

--- FrontEnd/src/components/Distribute/ComponentD1.jsx
+++ FrontEnd/src/components/Distribute/ComponentD1.jsx
@@ -3,51 +3,55 @@ import {Link} from "react-router-dom"
 import callIcon from '../../assets/callIcon2.svg'
 import dots from '../../assets/dot.svg'
 import distbottle from '../../assets/Distriburtbottle.svg'
+import bottle from '../../assets/distBottles.svg'
 // import { banner } from "../../assets/banner.png";
 
 function ComponentD1({ handleScrollToForm }) {
   return (
     <div className="w-full flex flex-col-reverse text-zinc-950 p-4 sm:p-16 md:flex-row md:justify-between relative">
       <div className="flex flex-col justify-start  w-full md:px-10">
-        <h1 className="w-full text-[40px] sm:text-4xl md:text-6xl lg:text-8xl font-extrabold text-zinc-950 mt-10 text-start md:text-center" >
+        <h1 className="w-full text-[40px] sm:text-6xl md:text-6xl  xl:text-8xl font-extrabold text-zinc-950 mt-10 lg:text-start text-center" >
           Distribute
           <br/>
           FreeWater
         </h1>
-        <p className="w-[311px] text-center sm:text-start sm:w-full py-10 text-[10px] sm:text-xl">
+        <p className="md:w-full lg:w-[600px] xl:w-[800px] text-center lg:text-start sm:w-full py-4 sm:py-6 md:py-6 xl:py-10 text-[10px] sm:text-[14px] md:text-[16px] lg:text-xl">
           Fill up your stores your parties, events , business gatherings
-          <br />
+          <br className="hidden xl:block" />
           with FreeWater.
         </p>
-        <div>
+        <div className="flex lg:flex-col justify-center items-center lg:items-start gap-1">
           <ul>
             <li>
-              <div className="flex justify-start py-6">
+              <div className="flex justify-start py-3 md:py-4 xl:py-6">
                 <img src={dots} className="w-[16px] sm:w-[18px] md-w-auto"></img>
-                <p className="px-3 text-[8px] md:text-xl">Increase Foot Traffic</p>
+                <p className="px-3 text-[8px] sm:text-[12px] md:text-xl">Increase Foot Traffic</p>
               </div>
             </li>
             <li>
-              <div className="flex justify-start py-6">
+              <div className="flex justify-start py-3 md:py-4 xl:py-6">
                 <img src={dots} className="w-[16px] sm:w-[18px] md-w-auto"></img>
-                <p className="px-3 text-[8px] md:text-xl">Improve Sales</p>
+                <p className="px-3 text-[8px] sm:text-[12px] md:text-xl">Improve Sales</p>
               </div>
             </li>
             <li>
-              <div className="flex justify-start py-6">
+              <div className="flex  justify-start py-3 md:py-4 xl:py-6">
                 <img src={dots} className="w-[16px] sm:w-[18px] md-w-auto"></img>
-                <p className="px-3 text-[8px] md:text-xl">Gain Amazing PR</p>
+                <p className="px-3 text-[8px] sm:text-[12px] md:text-xl">Gain Amazing PR</p>
               </div>
             </li>
           </ul>
-          <Link to="/contact-us">
-              <button className="bg-transparent hover:bg-gray-700 rounded-full w-[85.3px] sm:w-auto mt-[16px]">
-                <img src={callIcon}></img>
+          <Link to="/contactus">
+              <button className="bg-transparent hover:bg-gray-700 rounded-full w-[85.3px] sm:w-auto mt-[16px] hidden lg:block cursor-pointer z-20">
+                <img src={callIcon} className="w-[120px] lg:w-[140px] xl:w-auto"></img>
               </button>
           </Link>
+          <div className="lg:hidden">
+            <img src={bottle} className="sm:w-[330px]"></img>
+          </div>
         </div>
-        <div className="absolute right-0 top-0 hidden md:block">
-          <img src={distbottle}></img>
+        <div className="absolute right-0 top-0 hidden w-[50%] lg:flex md:justify-end z-0">
+          <img src={distbottle} className="w-[62%]"></img>
         </div>
       </div>
     </div>
